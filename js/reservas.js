b1d2ced0af2906edb59cda4c5c6b8be581ac53eb
muryllohenriq <muryllo.henrique@hotmail.com>
2022-08-18 19:03:23 -0300

    att

--- js/reservas.js
+++ js/reservas.js
@@ -1,24 +1,7 @@
-const SOUND_URL = 'https://xp41-soundgarden-api.herokuapp.com/events';
-
-const findID = () => {
-    const url = new URL(window.location.href);
-    const id = url.searchParams.get('id');
-
-    return id;
-}
-
-const exibirDetalhesEvento = async () => {
-    const dadosEvento = await fetch(SOUND_URL + "/" + findID(), {
-    method: "GET",
-    mode: "cors",
-    headers: {
-        "Content-Type": "application/json"
-    }
-    }).then((response) => response.json());
-}
+const SOUND_URL = 'https://xp41-soundgarden-api.herokuapp.com/bookings';
 
 const listarReservas = async () => {
-    const eventos = await fetch(SOUND_URL, {
+    const reservas = await fetch(SOUND_URL, {
       method: "GET",
       mode: "cors",
       headers: {
@@ -33,23 +16,23 @@ const listarReservas = async () => {
   
     const tbody = document.querySelector(".lista-reservas tbody");
   
-    let htmlEventos = "";
+    let htmlReservas = "";
   
-    eventos.forEach((evento) => {
-      htmlEventos += `
+    reservas.forEach((reserva) => {
+      htmlReservas += `
           <tr>
-                    <th scope="row">${evento.numero}</th>
-                    <td>${evento.cliente}</td>
-                    <td>${evento.name}</td>
+                    <th scope="row">${reserva.numero}</th>
+                    <td>${reserva.cliente}</td>
+                    <td>${reserva.name}</td>
                     <td></td>
                     <td>          
-                      <a href="excluir-evento.html?id=${evento._id}" class="btn btn-danger">excluir</a>
+                      <a href="excluir-reserva.html?id=${reserva._id}" class="btn btn-danger">excluir</a>
                     </td>
                   </tr>
           `;
     });
 
-  tbody.innerHTML = htmlEventos;
+  tbody.innerHTML = htmlReservas;
   
   };
 
